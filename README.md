c68ec95b9b2b1d062eb81de16360068a27f64f69
KapyPara <ahmed96s@hotmail.com>
2025-04-22 22:19:52 +0300

    Added docker build and readme. Updated next config for static build.

---
+++ README.md
@@ -0,0 +1,10 @@
+
+To build docker image
+
+`docker build -t social-network .`
+`docker run -p 8080:8080 --rm -it social-network`
+
+For a quick test
+
+`sudo docker build -t social-network . && sudo docker run -p 8080:8080 --rm -it
+ social-network`
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -50,6 +51,9 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <nav>
+          <Link href="/about">Go to About Page</Link>h
+        </nav>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
